c98e3a09f9ac6ec4e1a294a14bcff2e69b222dc3
lucas simonini <lcs.simonini@gmail.com>
2015-08-02 18:29:45 -0300

    primeiro commit

---
+++ crmEJB/ejbModule/br/com/simonini/models/Cliente.java
@@ -0,0 +1,13 @@
+package br.com.simonini.models;
+
+
+public class Cliente {
+
+    /**
+     * Default constructor. 
+     */
+    public Cliente() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ crmEJB/ejbModule/br/com/simonini/models/Contato.java
@@ -0,0 +1,13 @@
+package br.com.simonini.models;
+
+
+public class Contato {
+
+    /**
+     * Default constructor. 
+     */
+    public Contato() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
